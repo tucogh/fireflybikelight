db95ab517e6b1c1ae2021c61465fe7927b153ff6
phoenixblow5 <phoenixblow5@a4d6e750-1731-3668-0a12-2d7eef68dc7b>
2010-07-14 17:51:08 +0000

    d

---
+++ fireflybikelight/nbproject/Makefile-Release.mk
@@ -0,0 +1,73 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-MacOSX
+CND_CONF=Release
+CND_DISTDIR=dist
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES=
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-Release.mk dist/Release/GNU-MacOSX/fireflybikelight
+
+dist/Release/GNU-MacOSX/fireflybikelight: ${OBJECTFILES}
+	${MKDIR} -p dist/Release/GNU-MacOSX
+	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/fireflybikelight ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r build/Release
+	${RM} dist/Release/GNU-MacOSX/fireflybikelight
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
---
+++ fireflybikelight/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=fireflybikelight
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/fireflybikelight
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=fireflybikelight.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/fireflybikelight.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=fireflybikelight
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/fireflybikelight
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=fireflybikelight.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/fireflybikelight.tar
